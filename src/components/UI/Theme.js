16afc335cdc043ff1a7f397735df3055ee67028f
unknown <fuzailafsar92@gmail.com>
2023-02-27 05:48:30 +0500

    font family added

--- src/components/UI/Theme.js
+++ src/components/UI/Theme.js
@@ -1,5 +1,6 @@
 import { ConfigProvider } from "antd";
 import "antd/dist/reset.css";
+import "./main.css";
 
 const Theme = ({ children }) => (
   <ConfigProvider theme={{ token: { colorPrimary: "#1677ff" } }}>
