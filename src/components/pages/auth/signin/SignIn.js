0c87051da7987ab1faaa13f17c3c089d7f50bcef
Abdul Hadi Waseem <106449444+Abdul-Hadi-Waseem@users.noreply.github.com>
2023-03-03 12:35:22 +0500

    Login Created

--- src/components/pages/auth/signin/SignIn.js
+++ src/components/pages/auth/signin/SignIn.js
@@ -1,5 +1,41 @@
+import { Anchor, Button, Input } from "antd";
+
 const SignIn = () => {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+      <div className="cart">
+        <div className="cart__header">
+          <h2>Account</h2>
+        </div>
+
+        <section className="cart__main">
+          <div className="signin">
+            <h2>Login</h2>
+            <Input className="signin_child " placeholder="Email" />
+            <Input className="signin_child " placeholder="Password" />
+
+            <Anchor
+              showInkInFixed={"false"}
+              className="signin_child "
+              items={[
+                { key: "forget-pass", href: "/", title: "Forgot Password?" },
+              ]}
+            />
+
+            <Button type="primary" className="primary-btn2 center">
+              Sign In
+            </Button>
+            <Anchor
+              className="signin_child "
+              items={[
+                { key: "create-acc", href: "/", title: "Create Account" },
+              ]}
+            />
+          </div>
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default SignIn;
